8b7e9fbef0e2a904a891f0e57cef89a20cc2f1f8
The_DanKadlec <159142575+The-DanKadlec@users.noreply.github.com>
2025-05-19 18:33:46 +0800

    Update run_app.R

--- R/run_app.R
+++ R/run_app.R
@@ -1,9 +1,6 @@
 #' Launch the Shiny App
 #'
-#' This function launches the shinyForce3D application.
-#'
 #' @export
 run_app <- function() {
-  source(system.file("R", "shinyForce3D.R", package = "shinyForce3D"), local = TRUE)
-  shiny::shinyApp(ui = ui, server = server)
+  shiny::runApp(system.file("app", package = "shinyForce3D"))
 }
